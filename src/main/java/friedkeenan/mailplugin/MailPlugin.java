d0bd3aba184bbeda2ce75f7fcf8d06fa3c25a860
friedkeenan <friedkeenan@protonmail.com>
2018-12-09 18:30:26 -0600

    Initial commit

---
+++ src/main/java/friedkeenan/mailplugin/MailPlugin.java
@@ -0,0 +1,10 @@
+package friedkeenan.mailplugin;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class MailPlugin extends JavaPlugin{
+	@Override
+	public void onDisable() {}
+	@Override
+	public void onEnable() {}
+}
