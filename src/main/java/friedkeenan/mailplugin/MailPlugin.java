2afa7a511b991e5a91a9fd2abbbb1494c15a185d
friedkeenan <friedkeenan@protonmail.com>
2018-12-10 19:26:36 -0600

    Added preliminary right click event

--- src/main/java/friedkeenan/mailplugin/MailPlugin.java
+++ src/main/java/friedkeenan/mailplugin/MailPlugin.java
@@ -1,10 +1,25 @@
 package friedkeenan.mailplugin;
 
+import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.block.Action;
+import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
-public class MailPlugin extends JavaPlugin{
+public class MailPlugin extends JavaPlugin implements Listener{
 	@Override
 	public void onDisable() {}
 	@Override
-	public void onEnable() {}
+	public void onEnable() {
+		Bukkit.getServer().getPluginManager().registerEvents(this, this);
+	}
+	@EventHandler
+	public void openEnvelope(PlayerInteractEvent e) {
+		Player p=e.getPlayer();
+		if(e.getAction()==Action.RIGHT_CLICK_AIR || e.getAction()==Action.RIGHT_CLICK_BLOCK) {
+			p.sendMessage("Right click");
+		}
+	}
 }
