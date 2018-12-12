0add82e5cb152815f34139495ac3498fc9a381bf
friedkeenan <friedkeenan@protonmail.com>
2018-12-11 20:47:55 -0600

    Refactored and added some inventory stuff

--- src/main/java/friedkeenan/mailplugin/Envelope.java
+++ src/main/java/friedkeenan/mailplugin/Envelope.java
@@ -3,24 +3,42 @@ package friedkeenan.mailplugin;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.bukkit.Bukkit;
 import org.bukkit.Material;
 import org.bukkit.entity.Player;
+import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
 
-public class Envelope extends ItemStack{
-	Envelope(Player p){
-		super(Material.PAPER);
-		ItemMeta meta=getItemMeta();
+public class Envelope{
+	private ItemStack item;
+	private Inventory contents;
+	Envelope(ItemStack env) throws Exception{
+		if(!Envelope.isEnvelope(env)) {
+			throw new Exception("ItemStack is not an envelope");
+		}
+		item=env;
+		contents=Bukkit.createInventory(null, 9);
+		contents.addItem(new ItemStack(Material.STAINED_GLASS_PANE,1,(short)13));
+	}
+	static void giveEnvelope(Player p){
+		ItemStack stack=new ItemStack(Material.PAPER);
+		ItemMeta meta=stack.getItemMeta();
 		meta.setDisplayName("Unsealed Envelope");
 		List<String> lore=new ArrayList<String>();
 		lore.add("Envelope");
 		lore.add("Unsealed");
 		meta.setLore(lore);
-		setItemMeta(meta);
-		p.getInventory().addItem(this);
+		stack.setItemMeta(meta);
+		p.getInventory().addItem(stack);
 	}
 	static boolean isEnvelope(ItemStack stack) {
 		return stack.getType()==Material.PAPER && stack.getItemMeta().getLore().get(0)=="Envelope";
+	}
+	boolean isSealed() {
+		return item.getItemMeta().getLore().get(1)=="Sealed";
+	}
+	void open(Player p) {
+		p.openInventory(contents);
 	}
 }
--- src/main/java/friedkeenan/mailplugin/MailPlugin.java
+++ src/main/java/friedkeenan/mailplugin/MailPlugin.java
@@ -1,5 +1,8 @@
 package friedkeenan.mailplugin;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.bukkit.Bukkit;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
@@ -11,6 +14,7 @@ import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class MailPlugin extends JavaPlugin implements Listener{
+	List<Envelope> envelopes=new ArrayList<Envelope>();
 	@Override
 	public void onDisable() {}
 	@Override
@@ -20,14 +24,20 @@ public class MailPlugin extends JavaPlugin implements Listener{
 	@EventHandler
 	public void openEnvelope(PlayerInteractEvent e) {
 		Player p=e.getPlayer();
-		if(Envelope.isEnvelope(e.getItem()) && (e.getAction()==Action.RIGHT_CLICK_AIR || e.getAction()==Action.RIGHT_CLICK_BLOCK)) {
-			p.sendMessage("Right click");
+		if(e.getAction()==Action.RIGHT_CLICK_AIR || e.getAction()==Action.RIGHT_CLICK_BLOCK) {
+			if(e.getItem().getAmount()==1 && Envelope.isEnvelope(e.getItem())) {
+				try {
+					Envelope env=new Envelope(e.getItem());
+					env.open(p);
+				}catch(Exception exc) {}
+			}
 		}
 	}
 	@Override
 	public boolean onCommand(CommandSender sender,Command cmd,String label,String[] args) {
 		if(sender instanceof Player && cmd.getName().equalsIgnoreCase("env")) {
-			new Envelope((Player)sender);
+			Envelope.giveEnvelope((Player)sender);
+			return true;
 		}
 		return false;
 	}
